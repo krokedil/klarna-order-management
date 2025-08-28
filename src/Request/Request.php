711ebf52cdb4a31aa4b0b7def16b3cd0e26c8eb3
anyakro <anya@krokedil.se>
2025-08-28 08:56:31 +0200

    Redact sensitive information from the log

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -408,6 +408,13 @@ abstract class Request {
 	 * @return void
 	 */
 	protected function log_response( $response, $request_args, $code ) {
+		foreach ( $request_args['headers'] as $header => $value ) {
+			if ( 'authorization' === strtolower( $header ) ) {
+				// If it is longer than 15 char., it most likely has a token. This is an assumption that is safe even if it is wrong.
+				$request_args['headers'][ $header ] = strlen( $value ) > 15 ? '[REDACTED]' : '[MISSING]';
+				break;
+			}
+		}
 		$log = Logger::format_log( $this->klarna_order_id, $this->method, $this->log_title, $request_args, $response, $code );
 		Logger::log( $log, $this->order_management->plugin_instance, $this->order_id );
 	}
