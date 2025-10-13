1aa9bc626996df6757b4c2354411e47dbd6f1185
anyakro <anya@krokedil.se>
2025-10-13 16:01:50 +0200

    Add filter to enable skip scheduled actions

--- src/ScheduledActions.php
+++ src/ScheduledActions.php
@@ -43,6 +43,11 @@ class ScheduledActions {
 	 * @return void
 	 */
 	public static function print_scheduled_actions( $session_id ) {
+		// Check if a filter is applied to hide scheduled actions.
+		if ( apply_filters( 'kom_skip_scheduled_actions', false ) ) {
+			return;
+		}
+
 		$scheduled_actions = self::get_scheduled_actions( $session_id );
 		$session_query_url = admin_url(
 			'admin.php?page=wc-status&tab=action-scheduler&s=' . rawurlencode( $session_id ) . '&action=-1&paged=1&action2=-1'
